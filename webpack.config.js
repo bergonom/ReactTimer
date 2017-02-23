e782370296b2478c1901eb4204f855003c19f50d
Joel Berghoff <joel.berghoff@glassdoor.com>
2017-02-23 00:40:56 -0800

    Initial Commit

--- webpack.config.js
+++ webpack.config.js
@@ -23,13 +23,6 @@ module.exports = {
   resolve: {
     root: __dirname,
     alias: {
-      Main: 'app/components/Main.jsx',
-      Nav: 'app/components/Nav.jsx',
-      Timer: 'app/components/Timer.jsx',
-      Clock: 'app/components/Clock.jsx',
-      Controls: 'app/components/Controls.jsx',
-      Countdown: 'app/components/Countdown.jsx',
-      CountdownForm: 'app/components/CountdownForm.jsx',
       applicationStyles: 'app/styles/app.scss'
     },
     extensions: ['', '.js', '.jsx']
